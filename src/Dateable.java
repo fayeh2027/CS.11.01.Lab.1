114f048bcda15e0ea8839e3dff5a995e3ff440bc
fayeh2027 <fayeh2027@student.cis.edu.hk>
2024-08-24 19:58:48 +0800

    Update Dateable.java

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,9 @@
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Please enter your age");
+        int age = scanner.nextInt();
+        System.out.println("Your dateable age is " + (age/2+7) + ".");
     }
 }
