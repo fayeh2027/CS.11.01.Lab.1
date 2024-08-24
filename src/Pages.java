c819166bd94755d4b2d70c3e322f69d9d82ee48c
fayeh2027 <fayeh2027@student.cis.edu.hk>
2024-08-24 19:59:24 +0800

    Update Pages.java

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,9 @@
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Please enter your age");
+        int age = scanner.nextInt();
+        System.out.println( age + " year olds should read at least " + (100 - age) + " pages before giving up on a book.");
     }
 }
